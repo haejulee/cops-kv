57d5db527a2bed96b60ecafe0d60b5ef40715709
Julie Lee <haewon.lee@stonybrook.edu>
2020-12-07 17:45:39 +0000

    Lamport timestamp functions completed

--- src/copskv/server.go
+++ src/copskv/server.go
@@ -122,11 +122,16 @@ type KVSnapshot struct {
 	LastAppliedIndex int
 }
 
+// Extracts just the Lamport Timestamp from a version number
+func vtot(versionNum uint64) uint32 {
+	return uint32(versionNum >> 32)
+}
+
 // Returns true if v1 is at least as late as v2
 func versionUpToDate(v1, v2 uint64) bool {
 	// Convert to lamport timestamps
-	t1 := uint32(v1 >> 32)
-	t2 := uint32(v2 >> 32)
+	t1 := vtot(v1)
+	t2 := vtot(v2)
 	// compare timestamps
 	if t1 >= t2 {
 		return true
@@ -135,12 +140,25 @@ func versionUpToDate(v1, v2 uint64) bool {
 	}
 }
 
+// Returns Lamport timestamp of next event
+// Must hold lock while calling
 func (kv *ShardKV) lamportTimestamp() uint32 {
-	// TODO: implement a correct Lamport timestamp
+	// Add 1 to latest timestamp & return its value
 	kv.latestTimestamp += 1
 	return kv.latestTimestamp
 }
 
+// Updates system's latest witnessed timestamp,
+// considering an incoming timestamp
+// Must hold lock while calling
+func (kv *ShardKV) updateTimestamp(incoming uint32) {
+	if incoming > kv.latestTimestamp {
+		kv.latestTimestamp = incoming
+	}
+}
+
+// Returns a next version number based on Lamport Timestamp
+// Must hold lock while calling
 func (kv *ShardKV) versionNumber() uint64 {
 	timestamp := uint64(kv.lamportTimestamp())
 	ver := timestamp << 32
