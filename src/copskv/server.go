a489fc303f31b1a9e500ef7253c7e6085408c243
Julie Lee <haewon.lee@stonybrook.edu>
2020-12-07 18:46:18 +0000

    Complete async put request handling

--- src/copskv/server.go
+++ src/copskv/server.go
@@ -390,16 +390,21 @@ func (kv *ShardKV) apply(op Op) {
 			if version == 0 {
 				// If version argument was nil, generate new version number & apply immediately
 				version = kv.versionNumber()
+				kv.kvstore[shard][op.Key] = Entry{ version, op.Value, op.Nearest, false }
+				kv.lastApplied[op.ClientID] = CmdResults{ op, op.CommandID, op.Key, "", OK, version, op.Nearest, false }
 			} else {
 				// If version specified, do async put routine before applying get
 				// 1. make sure dependencies are satisfied
 				kv.doDepChecks(op.Nearest)
 				// 2. update latest timestamp using given version
 				kv.updateTimestamp(vtot(version))
+				// Apply put operation, only if new version is higher than existing
+				entry, ok := kv.kvstore[shard][op.Key]
+				if !ok || vtot(entry.Version) < vtot(version) ||
+				   (vtot(entry.Version) == vtot(version) && uint32(entry.Version) < uint32(version)) {
+					kv.kvstore[shard][op.Key] = Entry{ version, op.Value, op.Nearest, false }
+				}
 			}
-			// Apply put operation
-			kv.kvstore[shard][op.Key] = Entry{ version, op.Value, op.Nearest, false }
-			kv.lastApplied[op.ClientID] = CmdResults{ op, op.CommandID, op.Key, "", OK, version, op.Nearest, false }
 		}
 	case OpConfigChange:
 		kv.applyConfigChange(op.ConfigNum, op.ShardStore, op.LastApplied, op.Config)
@@ -614,7 +619,48 @@ func (kv *ShardKV) retrieveShard(shard int, shards *map[int]map[string]Entry, la
 
 // Blocks until all dependencies in deps are satisfied in the cluster
 func (kv *ShardKV) doDepChecks(deps map[string]uint64) {
-	// TODO
+	mu := sync.Mutex{}
+	count := 0
+	// Carry out dependency checks for each key concurrently
+	mu.Lock()
+	for key, version := range deps {
+		count += 1
+		go kv.doDepCheck(key, version, &mu, &count)
+	}
+	// Wait until all checks are done
+	for count > 0 {
+		mu.Unlock()
+		time.Sleep(10 * time.Millisecond)
+		mu.Lock()
+	}
+}
+
+func (kv *ShardKV) doDepCheck(key string, version uint64, mu *sync.Mutex, count *int) {
+	// Find group (& its servers) in charge of key
+	shard := key2shard(key)
+	latestConfig := kv.mck.Query(-1)
+	gid := latestConfig.Shards[shard]
+	servers := latestConfig.Groups[gid]
+	// Query servers in charge until success
+	for i := 0; ; {
+		srv := kv.make_end(servers[i])
+		args := DepCheckArgs{ key, version }
+		var reply DepCheckReply
+		ok := srv.Call("ShardKV.DepCheck", &args, &reply)
+		if !ok || reply.WrongLeader {
+			// If wrong leader or connection issue, try next server
+			i = (i+1) % len(servers)
+			continue
+		}
+		if reply.Ok {
+			mu.Lock()
+			*count -= 1
+			mu.Unlock()
+			return
+		}
+		// If not successful, rest & try again with same server
+		time.Sleep(time.Millisecond)
+	}
 }
 
 //
